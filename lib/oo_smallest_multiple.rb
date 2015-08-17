f7c514ebbb7c8c9dc70458e1b019d09c0ea082ed
apradhn <abhijit.pradhan6@gmail.com>
2015-08-17 10:02:14 -0400

    completes challenge

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,21 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+  attr_accessor :num
+  def initialize(num)
+    self.num = num
+  end
+
+  def lcm
+    m = 1
+    answer = 0
+    loop do 
+      multiple = num * m
+      if (1..num).to_a.all? {|n| multiple % n == 0}
+        answer = multiple
+        break
+      end
+      m = m + 1
+    end
+    answer    
+  end
+  
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,13 @@
-# Implement your procedural solution here!
+def smallest_multiple(num)
+  m = 1
+  answer = 0
+  loop do 
+    multiple = num * m
+    if (1..num).to_a.all? {|n| multiple % n == 0}
+      answer = multiple
+      break
+    end
+    m = m + 1
+  end
+  answer
+end
